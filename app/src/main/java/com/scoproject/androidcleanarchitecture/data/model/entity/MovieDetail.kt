e68b1fd9e3b6e51c493120aff00b0b3026b3cfb5
Ibnu Muzzakkir <personalict@gmail.com>
2018-06-05 22:12:29 +0700

    Add movie detail entitiy for show in ui movie detail page

---
+++ app/src/main/java/com/scoproject/androidcleanarchitecture/data/model/entity/MovieDetail.kt
@@ -0,0 +1,11 @@
+package com.scoproject.androidcleanarchitecture.data.model.entity
+
+/**
+ * Created by ibnumuzzakkir on 05/06/18.
+ * Mobile Engineer
+ */
+data class MovieDetail(
+       var movieTitle : String? = null,
+       var moviePosterPath: String? = null,
+       var movieDescription : String? = null
+)
