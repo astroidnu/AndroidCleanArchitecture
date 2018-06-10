940912658cd0a4624fbc62fa98fa896a6ed72f62
Ibnu Muzzakkir <personalict@gmail.com>
2018-06-11 06:17:05 +0700

    add main thread scheduler in app schedulers provider

--- app/src/main/java/com/scoproject/androidcleanarchitecture/external/scheduler/AppSchedulerProvider.kt
+++ app/src/main/java/com/scoproject/androidcleanarchitecture/external/scheduler/AppSchedulerProvider.kt
@@ -11,4 +11,5 @@ class AppSchedulerProvider : SchedulerProvider {
     override fun ui(): Scheduler = AndroidSchedulers.mainThread()
     override fun computation(): Scheduler = Schedulers.computation()
     override fun io(): Scheduler = Schedulers.io()
+    override fun mainThread(): Scheduler = AndroidSchedulers.mainThread()
 }
